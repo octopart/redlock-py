1be7c53bead25b96b376caf796ebb76e4216a2af
Mark Lindsay <mark@octopart.com>
2015-09-22 21:09:04 +0000

    octopart-2

--- setup.py
+++ setup.py
@@ -67,7 +67,7 @@ THE SOFTWARE.
 
 setup(
     name='redlock-py',
-    version='1.0.5-octopart-1',
+    version='1.0.5-octopart-2',
     packages=find_packages(),
     include_package_data=True,
     description='Redis locking mechanism',
